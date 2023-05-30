1c615a46bcf0711eead082aec0b7da79f3b3161c
marievtenaud <133208759+marievtenaud@users.noreply.github.com>
2023-05-30 18:23:20 +0200

    Update streamlit_app.py
    Delete Raw JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,5 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-
-# write your own comment - converts string to text
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - shows data
 streamlit.dataframe(fruityvice_normalized)
